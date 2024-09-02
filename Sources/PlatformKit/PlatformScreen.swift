a1e5ed5689ccb6b00881201ea9145e263c7f70df
fwcd <fwcd@fwcd.dev>
2024-09-02 03:35:47 +0200

    Add PlatformScreen

---
+++ Sources/PlatformKit/PlatformScreen.swift
@@ -0,0 +1,19 @@
+#if canImport(UIKit) && !os(watchOS)
+
+import UIKit
+
+public typealias PlatformScreen = UIScreen
+
+#elseif canImport(AppKit)
+
+import AppKit
+
+public typealias PlatformScreen = NSScreen
+
+public extension NSScreen {
+    var scale: CGFloat {
+        backingScaleFactor
+    }
+}
+
+#endif
